1fedc886167496bc97531021a19b4ccdefcb0b7d
arnab2001 <63441472+arnab2001@users.noreply.github.com>
2023-03-12 21:18:47 +0530

    added event component

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <Navbar />
-            <Header />
+            <Header /> 
         </div>
     );
 }
