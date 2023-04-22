f6839be7970bd148c6ea1e74358164c0f5ff0a48
ABHIRUP <abhirupsaha02@gmail.com>
2023-04-22 15:38:54 +0530

    name changed to index.jsx from Hod.jsx

--- client/component/Hod/index.jsx
+++ client/component/Hod/index.jsx
@@ -1,6 +1,7 @@
 import styles from '../../styles/Hod.module.css';
 import Image from 'next/image';
 import Avatar from '../../assets/hod_img.jpg'
+
 const Hod = () => {
     return (
         <>
@@ -8,8 +9,7 @@ const Hod = () => {
                 <div className={styles.hod_container_wrap}>
 
                     <div className={styles.hod_image}>
-                        <Image src={Avatar} width={200} height={200} className={styles.hod_image_style}
-                        />
+                        <Image src={Avatar} width={200} height={200} className={styles.hod_image_style} alt=''/>
                         <div className={styles.hod_name}>PD SIR</div>
                     </div>
                     <div className={styles.line}></div>
