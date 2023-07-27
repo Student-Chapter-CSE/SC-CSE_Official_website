01c053b4e73339509a985d08cabbffd1c6e9c52b
soumit-s <soumit.srim@gmail.com>
2023-07-28 01:01:51 +0530

    Added EventDrawer component for the upcoming and past events section

---
+++ client/component/EventDrawer/index.js
@@ -0,0 +1,54 @@
+import styles from '../../styles/EventDrawer.module.css'
+
+import { useEffect, useState } from 'react'
+
+
+export default function EventDrawer({children}) {
+
+    const tabs = [
+        {name: 'Upcoming'},
+        {name: 'Past'}
+    ]
+
+    const [currentTabId, setCurrentTabId] = useState(0);
+
+    const wantTabChange = (toId) => {
+        if (toId == currentTabId)
+            return;
+        setCurrentTabId(toId)
+    }
+
+    return (
+        <div>
+            {/* Tabs */}
+            <nav className={styles.tabNav}>
+                {
+                    tabs.map((tab, i) => (
+                        <div className={styles.tab} 
+                            key={i} 
+                            onClick={() => wantTabChange(i)}
+                            data-active={currentTabId == i}
+                        >
+                            {tab.name}
+                        </div>
+                    ))
+                }        
+            </nav>
+
+            {/* Content  */}
+            <div className={styles.drawers}>
+                {
+                    children && [...children].map((c, i) => (
+                        <div 
+                            className={styles.drawer}
+                            key={i}
+                            data-active={currentTabId == i}
+                        >
+                            {c}
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
