5d1877737e6d5552782fb0779c2493b8f614d40a
bishakhne0gi <bneogi102002@gmail.com>
2023-07-27 21:17:51 +0530

    feat: navbar & counter

--- client/component/Navbar/DeskTopNavbar.jsx
+++ client/component/Navbar/DeskTopNavbar.jsx
@@ -1,4 +1,5 @@
 import styles from '../../styles/Navbar.module.css';
+import Link from 'next/link'
 const DeskTopNavbar = () => {
     return (
         <>
@@ -8,10 +9,25 @@ const DeskTopNavbar = () => {
                     <p className={styles.Navlogopara}>CSE</p>
                 </div>
                 <div className={styles.NavMid}>
-                    <div className={styles.NavElement}>HOME</div>
-                    <div className={styles.NavElement}>TEAM</div>
+                    <div className={styles.NavElement}>
+                        <Link href='/'>
+
+                            HOME
+                        </Link>
+                    </div>
+                    <div className={styles.NavElement}>
+                        <Link href='/team'>
+
+                            TEAM
+                        </Link>
+                    </div>
                     {/* <div className={styles.NavElement}>GALLERY</div> */}
-                    <div className={styles.NavElement}>EVENTS</div>
+                    <div className={styles.NavElement}>
+                        <Link href='/events'>
+
+                            EVENTS
+                        </Link>
+                    </div>
                 </div>
                 <div className={styles.Navleft}>
                     <button className={styles.Navbtn}>Become a member</button>
--- client/component/Navbar/MobileNavbar.jsx
+++ client/component/Navbar/MobileNavbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { X, List } from '@phosphor-icons/react';
 import styles from '../../styles/Navbar.module.css';
-
+import Link from 'next/link'
 const MobileNavbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -26,9 +26,26 @@ const MobileNavbar = () => {
             {isOpen && (
                 <div className={styles.BlurBackground}>
                     <ul className={styles.Menu_items}>
-                        <li>Home</li>
-                        <li>Team</li>
-                        <li>Event</li>
+                        <li>
+                            <Link href='/'>
+
+
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='/team'>
+
+
+                                Team
+                            </Link>
+
+                        </li>
+                        <li><Link href='/events'>
+
+
+                            Events
+                        </Link></li>
                     </ul>
                 </div>
             )}
