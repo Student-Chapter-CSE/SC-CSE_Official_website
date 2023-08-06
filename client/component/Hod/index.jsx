12c7fce61e70c0db8f8eb15716353ffcfab1fe7d
soumit-s <soumit.srim@gmail.com>
2023-08-06 20:39:07 +0530

    Added a dashed border around the image. Moved the name below the message and added a quotation mark to make it resemble a blockquote.

--- client/component/Hod/index.jsx
+++ client/component/Hod/index.jsx
@@ -9,16 +9,20 @@ const Hod = () => {
                 <div className={styles.hod_container_wrap}>
 
                     <div className={styles.hod_image}>
-                        <Image src={Avatar} width={200} height={200} className={styles.hod_image_style} alt=''/>
-                        <div className={styles.hod_name}>PD SIR</div>
+                        <Image src={Avatar} width={200} height={200} className={styles.hod_image_style} alt='' />
                     </div>
                     <div className={styles.line}></div>
                     <div className={styles.hod_right}>
                         <div className={styles.hod_title}>
-                            A note from the SC CSE head
+                            A note from the SC CSE Head
                         </div>
-                        <div className={styles.hod_desciption}>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo dignissimos facere impedit provident quod eaque nihil, at magni perspiciatis sapiente consectetur similique nisi inventore molestiae itaque illo corporis iusto veritatis.
+                        <div className={styles.hod_body}>
+                            <div>
+                                <div className={styles.hod_desciption}>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo dignissimos facere impedit provident quod eaque nihil, at magni perspiciatis sapiente consectetur similique nisi inventore molestiae itaque illo corporis iusto veritatis.
+                                </div>
+                                <div className={styles.hod_name}>Sir Prasenjit Das</div>
+                            </div>
                         </div>
                     </div>
                 </div>
