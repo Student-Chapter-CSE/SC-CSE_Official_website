b7fab74480b6e5b25f6de5685f2eb0836c6e6ed5
soumit-s <soumit.srim@gmail.com>
2023-08-06 20:42:59 +0530

    Background becomes solid and a border appears at the bottom, when scrolled beyond the hero section. Changed the hover design of the navigation links. Decreased border radius and transition duration of 'Become a member' button. Changed 'CSE' color in the logo to CSS variable `textColor`.

--- client/component/Navbar/DeskTopNavbar.jsx
+++ client/component/Navbar/DeskTopNavbar.jsx
@@ -1,9 +1,31 @@
 import styles from '../../styles/Navbar.module.css';
 import Link from 'next/link'
+
+import { useState, useEffect } from 'react';
+
 const DeskTopNavbar = () => {
+
+    const [shouldMorph, setMorph] = useState(true);
+
+    useEffect(() => {
+
+        const trigger = document.createElement('div')
+        trigger.style.position = 'absolute';
+        trigger.style.top = '0';
+        trigger.style.height = '100vh';
+        document.body.appendChild(trigger)
+
+        const o = new IntersectionObserver(([e]) => {
+            setMorph(!e.isIntersecting)
+        })
+
+        o.observe(trigger)
+        return () => o.unobserve(trigger)
+    }, [])
+
     return (
         <>
-            <div className={styles.NavbarContainer}>
+            <div className={styles.NavbarContainer} data-morphed={shouldMorph}>
                 <div className={styles.NavLogoContainer}>
                     <div className={styles.NavLogo}>Student's Chapter</div>
                     <p className={styles.Navlogopara}>CSE</p>
