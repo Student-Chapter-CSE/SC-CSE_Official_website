a10a8ad5f5c94d00f84516e598113a7991b70444
soumit-s <soumit.srim@gmail.com>
2023-08-09 02:32:14 +0530

    Added footer and made the page responsive.

--- client/pages/team.js
+++ client/pages/team.js
@@ -1,14 +1,15 @@
 import Navbar from "../component/Navbar";
 import Teams from "../component/Teams";
 
+import Footer from '../component/Footer'
+
 
 const team = () => {
     return (
         <>
-
             <Navbar />
             <Teams />
-
+            <Footer />
         </>
     );
 };
