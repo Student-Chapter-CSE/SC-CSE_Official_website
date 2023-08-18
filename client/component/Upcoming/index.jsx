52b6ce00350a058f53fea6f3d68afc0f8a3ca3b0
soumit-s <soumit.srim@gmail.com>
2023-08-18 22:19:41 +0530

    Complete overhaul of the upcoming events section.

--- client/component/Upcoming/index.jsx
+++ client/component/Upcoming/index.jsx
@@ -1,4 +1,8 @@
 import styles from '../../styles/Upcoming.module.css';
+import Link from 'next/link';
+import useMediaQuery from '../../hooks/useMediaQuery';
+import { SponsorList } from '../Sponsor';
+import { useEffect, useRef } from 'react';
 
 export default function UpcomingEvents({ events }) {
     return (
@@ -8,8 +12,9 @@ export default function UpcomingEvents({ events }) {
                     What's Upcoming
                 </div>
                 <div className={styles.upcomingEventsMainText}>
-                    <h2>MARK YOUR CALENDARS!</h2>
+                    <h2>MARK YOUR CALENDARS</h2>
                 </div>
+                <div className={styles.divider}></div>
                 <div className={styles.upcomingBody}>
                     {
                         events.length ?
@@ -33,6 +38,13 @@ export default function UpcomingEvents({ events }) {
 }
 
 function Card({ details, descriptionMaxLength = 200 }) {
+    const contentRef = useRef()
+
+    const isMedScreen = useMediaQuery('(min-width: 768px)')
+
+    useEffect(() => {
+        contentRef.current.innerHTML = details.content
+    })
 
     function nth(d) {
         const dString = String(d)
@@ -57,17 +69,50 @@ function Card({ details, descriptionMaxLength = 200 }) {
 
     return (
         <div className={styles.card}>
-            <div className={styles.cardTitle} onClick={() => details && details.link && gotoLink(details.link)}>
-                {details && details.title}
+            <div className={styles.cardImg}>
+                <img src='/static/assets/ih_1.png'></img>
             </div>
-            <div className={styles.cardDate}>{
-                details && details.date && dateToText(details.date)
-            }</div>
-            <div className={styles.cardDesc}>
-                {details && details.desc &&
-                    details.desc.length > descriptionMaxLength &&
-                    details.desc.substring(0, descriptionMaxLength) + '...' ||
-                    details.desc}
+            <div className={styles.cardContent}>
+                <div className={styles.cardContent_Left}>
+                    <div>
+                        <div className={styles.cardTitle} onClick={() => details && details.link && gotoLink(details.link)}>
+                            {details && details.heading}
+                        </div>
+                        <div className={styles.cardDate}>{
+                            details && details.date && dateToText(details.date)
+                        }</div>
+                    </div>
+                    {
+                        isMedScreen && details.sponsors && <CardSponsorSection sponsors={details.sponsors}/>
+                    }
+                </div>
+                <div className={styles.cardContent_Right}>
+                    <div ref={contentRef} className={styles.cardEventDesc}>
+
+                    </div>
+                    <div className={styles.learnMore}>
+                        <Link href="#">Learn More</Link>
+                    </div>
+                </div>
+                {
+                        !isMedScreen && details.sponsors && 
+                        <div>
+                            <CardSponsorSection sponsors={details.sponsors}/>
+                        </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+function CardSponsorSection({ sponsors }) {
+    return (
+        <div className={styles.sponsorListSection}>
+            <div className={styles.sponsorListHeading}>
+                Our Sponsors
+            </div>
+            <div className={styles.sponsorList}>
+                <SponsorList sponsors={sponsors} />
             </div>
         </div>
     )
