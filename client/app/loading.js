fd144b962bfb94527818b14f9377271c9f98e836
Ankan Pal <itsyourap@gmail.com>
2024-04-21 14:14:48 +0530

    Fix Preloader responsiveness

--- client/app/loading.js
+++ client/app/loading.js
@@ -1,7 +1,12 @@
+'use client';
+
 import Preloader from '../component/Preloader';
+import useMediaQuery from '../hooks/useMediaQuery';
 
 const Loading = ({ suspense }) => {
-    return <Preloader width="600px" height="600px" suspense={suspense} />;
+    const isDesktop = useMediaQuery('(min-width:700px)');
+    const side = isDesktop ? "600px" : "300px";
+    return <Preloader width={side} height={side} suspense={suspense} />
 };
 
 export default Loading;
